071ec1368cabcce48dc5019dad2bf67c17e063f7
Nicolò <nicolo.frison.1@studenti.unipd.it>
2019-12-05 20:58:39 +0100

    Implementata la classe MenuItem

--- sandwich-shop-manager/src/main/java/it/unipd/tos/model/MenuItem.java
+++ sandwich-shop-manager/src/main/java/it/unipd/tos/model/MenuItem.java
@@ -4,5 +4,35 @@
 package it.unipd.tos.model;
 
 public class MenuItem {
+    public enum products{Panini, Fritti, Bevande};
     
+    private products type;
+    private String name;
+    private double price;
+    
+    public MenuItem(products type, String name, double price) {
+        this.type = type;
+        if( name.equals("") ) {
+            throw new IllegalArgumentException("Il nome non può essere vuoto");
+        } else {
+            this.name = name;
+        }
+        if( price <= 0 ) {
+            throw new IllegalArgumentException("Il prezzo non può essere inferiore o uguale a 0");
+        } else {
+            this.price = price;
+        }
+    }
+    
+    public products getType() {
+        return this.type;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    public double getPrice() {
+        return this.price;
+    }
 }
