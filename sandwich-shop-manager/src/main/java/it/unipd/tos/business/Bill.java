2e0ebe651db39289f03549451b3d468a12378dcc
Nicolò <nicolo.frison.1@studenti.unipd.it>
2019-12-06 08:37:09 +0100

    Aggiunto sconto sul panino con prezzo minore
    Aggiunto sconto del 50% sul panino con prezzo minore se il numero di panini nell'ordine è maggiore di 5

--- sandwich-shop-manager/src/main/java/it/unipd/tos/business/Bill.java
+++ sandwich-shop-manager/src/main/java/it/unipd/tos/business/Bill.java
@@ -11,11 +11,34 @@ import it.unipd.tos.model.MenuItem;
 public class Bill implements TakeAwayBill {
     public double getOrderPrice(List<MenuItem> itemsOrdered) throws TakeAwayBillException {
         double total = 0;
+        int nPanini = 0;
         
         for( MenuItem i : itemsOrdered ) {
             total += i.getPrice();
         }
         
+        total -= this.getLowestPricePaninoDiscount(itemsOrdered);
+        
         return total;
+    }
+    
+    private double getLowestPricePaninoDiscount(List<MenuItem> itemsOrdered) {
+        double discount = Double.MAX_VALUE;
+        int nPanini = 0;
+        
+        for( MenuItem i : itemsOrdered ) {
+            if( i.getType() == MenuItem.products.Panini ) {
+                nPanini++;
+                discount = i.getPrice() < discount ? i.getPrice() : discount;
+            }
+        }
+        
+        if( nPanini > 5 ) {
+            discount /= 2;
+        } else {
+            discount = 0;
+        }
+        
+        return discount;
     }
 }
